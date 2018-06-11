4128c86bbe61575b12e6778cd20fd3aa365ac37b
Woojun Choi <tim.woojune1@gmail.com>
2018-06-11 14:39:34 -0700

    basic structure added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function App(){
+  return (
+      //Used React-Router to put part 1 and 2 together on one App
+      <div className='container'>
+
+      </div>
+  )
+}
+
+
+export default App;
---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "react-dom";
+import App from "./App";
+import { HashRouter } from "react-router-dom";
+
+render(
+  <HashRouter>
+    <App />
+  </HashRouter>,
+  document.getElementById("container")
+);
