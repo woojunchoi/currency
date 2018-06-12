0a0aad484bf390a19e18dd21f60d47629166e445
Woojun Choi <tim.woojune1@gmail.com>
2018-06-12 01:09:06 -0700

    small css fix

--- src/Main.js
+++ src/Main.js
@@ -4,7 +4,7 @@ import App from "./App";
 
 render(
     // added inline styling to reduce number of css files
-    <div style={{width:'100%', display:'flex', justifyContent:'center', margin:'0 auto', backgroundColor:'#f1f2ed'}}>
+    <div style={{width:'100%', display:'flex', justifyContent:'center', margin:'0 auto'}}>
         <App />
     </div>,
   document.getElementById("container")
