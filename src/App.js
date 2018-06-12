cb98848da64f9f380e6a3ec07b23ba94e306af1d
Woojun Choi <tim.woojune1@gmail.com>
2018-06-12 00:33:42 -0700

    comments added

--- src/App.js
+++ src/App.js
@@ -16,15 +16,15 @@ class App extends Component {
         }
     }
 
+    //grab the changed values from startdate-input and modify state with the values
     startChange = (e) => {
         this.setState({
             startMonth: parseInt(e.target.value.slice(0, 2)),
             startDay: parseInt(e.target.value.slice(3, 5)),
             startYear: parseInt(e.target.value.slice(6))
         })
-        console.log(this.state)
     }
-
+    //grab the changed values from enddate-input and modify state with the values
     endChange = (e) => {
         this.setState({
             endMonth: parseInt(e.target.value.slice(0, 2)),
@@ -32,14 +32,13 @@ class App extends Component {
             endYear: parseInt(e.target.value.slice(6))
         })
     }
-
+    //this method will get invoked when user clicks submit button. 
     submit = () => {
-        console.log(
-            'getting here?'
-        )
+
+        //checks if startyear is bigger than endyear
         if (this.state.startYear > this.state.endYear) {
             alert('start year value has to be less than end year')
-            console.log(this.state.endYear, this.state.startYear)
+            //if it fails the test, reset state and enddate input
             this.setState({
                 endYear: 0,
                 endDay: 0,
@@ -48,8 +47,11 @@ class App extends Component {
             this.refs.endInput.value = ''
             return;
         }
+        //check if startmonth is bigger than endmonth (startyear has to be equal or smaller)
         if (this.state.startMonth > this.state.endMonth && this.state.startYear >= this.state.endYear) {
             alert('start month value has to be less than end month')
+            //if it fails the test, reset state and enddate input
+
             this.setState({
                 endYear: 0,
                 endDay: 0,
@@ -58,8 +60,11 @@ class App extends Component {
             this.refs.endInput.value = ''
             return;
         }
+        //check if startday is equal or smaller (also check year and month)
         if (this.state.startDay >= this.state.endDay && this.state.startYear >= this.state.endYear && this.state.startMonth >= this.state.endMonth) {
             alert('start day value has to be less than end day')
+            //if it fails the test, reset state and enddate input
+
             this.setState({
                 endYear: 0,
                 endDay: 0,
@@ -68,9 +73,11 @@ class App extends Component {
             this.refs.endInput.value = ''
             return;
         }
-        console.log(this.state)
+
+        //used http get method because the data doesn't need to be secure (passed variables as params)
         return axios.get(`/calculate/${this.state.startYear}/${this.state.startMonth}/${this.state.startDay}/${this.state.endYear}/${this.state.endMonth}/${this.state.endDay}`)
             .then((response) => {
+                //update state with the value from the server
                 this.setState({
                     totalCost: response.data
                 })
@@ -85,11 +92,13 @@ class App extends Component {
             <div className='container'>
                 <h1>Bob's <span className='banana'>Banana</span> Budget</h1>
                 <p>Start Date (MM/DD/YYYY)</p>
+                {/* used regex to receive data with correct format */}
                 <input className='date-input' minLength='10' maxLength='10'
                     required='required' type="text" name="input" placeholder="MM-DD-YYYY"
                     onChange={(e) => this.startChange(e)} required pattern="(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d" />
 
                 <p>End Date (MM/DD/YYYY)</p>
+                {/* used regex to receive data with correct format */}
                 <input className='date-input' minLength='10' maxLength='10'
                     required='required' ref='endInput' type="text" name="input" placeholder="MM-DD-YYYY"
                     onChange={(e) => this.endChange(e)} required pattern="(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d" />
--- src/Main.js
+++ src/Main.js
@@ -3,6 +3,7 @@ import { render } from "react-dom";
 import App from "./App";
 
 render(
+    // added inline styling to reduce number of css files
     <div style={{width:'100%', display:'flex', justifyContent:'center', margin:'0 auto', backgroundColor:'#f1f2ed'}}>
         <App />
     </div>,
